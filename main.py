ed8a9688c1fb9ceccad1df2856de144fefc36502
cameronneylon <cn@cameronneylon.net>
2021-04-11 12:39:15 +0100

    Initial gh-pages commit

--- main.py
+++
@@ -1,8 +0,0 @@
-import shutil
-import modules.analytics
-from precipy.main import render_file
-
-render_file('config.json', [modules.analytics], storages=[])
-
-# Copy file to top level for gh-pages deployment
-shutil.copy('precipy/acs.html', 'index.html')
